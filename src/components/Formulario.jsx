282391efb7e16dd6b94e6702dc59468577dc2831
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-15 19:35:04 -0500

    form UI

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 const Formulario = () => {
   return (
-    <div>
-      <div>Aqui</div>
-    </div>
+    <form action="" className="col-md-12">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por categoría o ingrediente</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Buscar por ingrediente..."
+          />
+        </div>
+        <div className="col-md-4">
+          <select id="" className="form-control" name="categoria">
+            <option value="">--Seleccione categoría--</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            value="Buscar"
+            className="btn btn-block btn-primary"
+          />
+        </div>
+      </div>
+    </form>
   );
 };
 
