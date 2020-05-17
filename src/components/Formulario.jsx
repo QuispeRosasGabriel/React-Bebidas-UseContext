a1f3965f3877e4e828f0a1223f5db2eb42c89876
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-17 17:46:39 -0500

    creando context para capturar data del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CategoriasContext } from "../context/CategoriasContext";
 
 const Formulario = () => {
+  const [busqueda, guardarBusqueda] = useState({
+    nombre: "",
+    categoria: "",
+  });
+
   //datos traidos del context
   const { categorias } = useContext(CategoriasContext);
+
+  //leer contenidos
+  const obtenerDatosReceta = (e) => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <form action="" className="col-md-12">
@@ -18,10 +31,16 @@ const Formulario = () => {
             name="nombre"
             className="form-control"
             placeholder="Buscar por ingrediente..."
+            onChange={obtenerDatosReceta}
           />
         </div>
         <div className="col-md-4">
-          <select id="" className="form-control" name="categoria">
+          <select
+            id=""
+            className="form-control"
+            name="categoria"
+            onChange={obtenerDatosReceta}
+          >
             <option value="">--Seleccione categoría--</option>
             {categorias.map((categoria) => (
               <option key={categoria.strCategory} value={categoria.strCategory}>
